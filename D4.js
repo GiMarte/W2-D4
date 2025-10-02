3b414ffb550674c8ba4c8279d2190f9a8a4f82b5
Hammers <gianluca.hammers@gmail.com>
2025-10-02 14:38:57 +0200

    Half exercises

--- D4.js
+++ D4.js
@@ -1,53 +1,82 @@
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 const area = function (l1, l2) {
   let areaRett = (l1 * l2) / 2;
   return areaRett;
 };
-console.log(area(10, 5))
-
+console.log("Area del triangolo:", area(10, 5));
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
  la loro somma moltiplicata per tre.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 const crazySum = function (num1, num2) {
-    
-}
+  if (num1 === num2) {
+    let moltiplication = (num1 + num2) * 3;
+    return moltiplication;
+  } else {
+    let addiction = num1 + num2;
+    return addiction;
+  }
+};
 
-
-
+console.log("Non inserire due numeri uguali altrimenti:", crazySum(100, 100));
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const crazyDiff = function (num) {
+  if (num > 19) {
+    let difference = (num - 19) * 3;
+    return difference;
+  } else {
+    let difference = num - 19;
+    return difference;
+  }
+};
+
+console.log("Fetish per il numero 19:", crazyDiff(50));
 
 /* ESERCIZIO 4
- Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
- se n è uguale a 400.
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure se n è uguale a 400.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const boundary = function (n) {
+  if (n > 20 && n <= 100) {
+    return true;
+  } else if (n === 400) {
+    let confermo400 = "confermato";
+    return confermo400;
+  } else {
+      let noIdea= "e non conosco " + n + " come numero"
+      return noIdea
+  }
+};
+console.log("so contare da 20 a 100, so dire 400" ,boundary(400));
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
  ritornare la stringa originale senza alterarla.
 */
-
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const epify = function (str) {
+    if (str === "epicode".toLowerCase) 
+
+ }
+
+console.log(epify("ciao"))
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
